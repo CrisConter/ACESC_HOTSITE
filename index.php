c224d22940e29a119a33ba43cb1372d721fc77c5
Cristiano Conter <crisconter@yahoo.com.br>
2012-11-13 14:51:40 -0200

    First Commit. Upload all files

---
+++ index.php
@@ -0,0 +1,437 @@
+<?php
+
+//TODO: add facebox plugin or another lightbox test it if fail (http://ryrych.github.com/rlightbox2/)
+//TODO: create the register form
+// inline label form http://www.zurb.com/playground/inline-form-labels
+// put on database
+//TODO: create the submit form to upload the document (http://www.zurb.com/playground/ajax_upload?)
+// how upload file on mobile device?
+//TODO: add plugin to share
+//TODO: develop back-end
+//TODO:
+?>
+
+<html>
+	<head>
+		<title>ACESC</title>
+		<link rel="stylesheet" href="css/style.css" />
+		<link rel="stylesheet" href="css/reveal.css" />
+
+	</head>
+	<body>
+		<div id="myModal" class="reveal-modal">
+			<h1>Inscreva-se</h1>
+			<form name="" action="" method="post">
+
+			</form>
+			<input type="text" name="name" id="name" title="Nome completo" value="" />
+			<input type="text" name="born" id="born" title="Data de nascimento" value="" />
+			<input type="mail" name="email" id="email" title="E-mail" value="" />
+			<input type="text" name="phone" id="phone" title="Telefone" value="" />
+			<input type="text" name="city" id="city" title="Cidade" value="" />
+            <input type="text" name="eventName" id="eventName" value="" />
+			<p>
+				Declaro que l&iacute; e aceito o <a href="#" target="_blank">regulamento</a>
+			</p>
+			<input type="button" value="Participar" />
+			<a class="close-reveal-modal">&#215;</a>
+		</div>
+		<head>
+			<div>
+				logo
+			</div>
+			<div>
+				newsletter
+			</div>
+			<div>
+				menus | menus | menus
+			</div>
+		</head>
+		<div id="container" class="clickable clearfix">
+
+			<div class="element opa alkaline-earth metal large" data-symbol="Mg" data-category="alkaline-earth">
+				<div class='littleContent'>
+					<p>pequeno</p>
+				</div>
+				<div class="bigContent">
+					<p>grande</p>
+                    <br /><br /><br /><br />
+                    <p><a href="#" data-reveal-id="myModal" data-event-name="Primeiro evento">Inscreva-se</a></p>
+				</div>
+			</div>
+
+			<div class="element opa actinoid metal inner-transition" data-symbol="U" data-category="actinoid">
+				<p class="number">
+					92
+				</p>
+				<h3 class="symbol">U</h3>
+				<h2 class="name">Uranium</h2>
+				<p class="weight">
+					238.02891
+				</p>
+			</div>
+
+			<div class="element opa lanthanoid metal inner-transition" data-symbol="Gd" data-category="lanthanoid">
+				<p class="number">
+					64
+				</p>
+				<h3 class="symbol">Gd</h3>
+				<h2 class="name">Gadolinium</h2>
+				<p class="weight">
+					157.25
+				</p>
+			</div>
+
+			<div class="element opa transition metal" data-symbol="Y" data-category="transition">
+				<p class="number">
+					39
+				</p>
+				<h3 class="symbol">Y</h3>
+				<h2 class="name">Yttrium</h2>
+				<p class="weight">
+					88.90585
+				</p>
+			</div>
+
+			<div class="element opa metalloid" data-symbol="B" data-category="metalloid">
+				<p class="number">
+					5
+				</p>
+				<h3 class="symbol">B</h3>
+				<h2 class="name">Boron</h2>
+				<p class="weight">
+					10.811
+				</p>
+			</div>
+
+			<div class="element opa transition metal" data-symbol="Fe" data-category="transition">
+				<p class="number">
+					26
+				</p>
+				<h3 class="symbol">Fe</h3>
+				<h2 class="name">Iron</h2>
+				<p class="weight">
+					55.845
+				</p>
+			</div>
+
+			<div class="element opa actinoid metal inner-transition" data-symbol="Am" data-category="actinoid">
+				<p class="number">
+					95
+				</p>
+				<h3 class="symbol">Am</h3>
+				<h2 class="name">Americium</h2>
+				<p class="weight">
+					(243)
+				</p>
+			</div>
+
+			<div class="element opa transition metal" data-symbol="Zn" data-category="transition">
+				<p class="number">
+					30
+				</p>
+				<h3 class="symbol">Zn</h3>
+				<h2 class="name">Zinc</h2>
+				<p class="weight">
+					65.38
+				</p>
+			</div>
+
+			<div class="element opa lanthanoid metal inner-transition" data-symbol="Pm" data-category="lanthanoid">
+				<p class="number">
+					61
+				</p>
+				<h3 class="symbol">Pm</h3>
+				<h2 class="name">Promethium</h2>
+				<p class="weight">
+					(145)
+				</p>
+			</div>
+
+			<div class="element opa actinoid metal inner-transition" data-symbol="Pu" data-category="actinoid">
+				<p class="number">
+					94
+				</p>
+				<h3 class="symbol">Pu</h3>
+				<h2 class="name">Plutonium</h2>
+				<p class="weight">
+					(244)
+				</p>
+			</div>
+
+			<div class="element opa transition metal" data-symbol="Au" data-category="transition">
+				<p class="number">
+					79
+				</p>
+				<h3 class="symbol">Au</h3>
+				<h2 class="name">Gold</h2>
+				<p class="weight">
+					196.966569
+				</p>
+			</div>
+
+			<div class="element opa transition metal" data-symbol="Mn" data-category="transition">
+				<p class="number">
+					25
+				</p>
+				<h3 class="symbol">Mn</h3>
+				<h2 class="name">Manganese</h2>
+				<p class="weight">
+					54.938045
+				</p>
+			</div>
+
+			<div class="element opa actinoid metal inner-transition" data-symbol="Es" data-category="actinoid">
+				<p class="number">
+					99
+				</p>
+				<h3 class="symbol">Es</h3>
+				<h2 class="name">Einsteinium</h2>
+				<p class="weight">
+					(252)
+				</p>
+			</div>
+
+			<div class="element opa actinoid metal inner-transition" data-symbol="Md" data-category="actinoid">
+				<p class="number">
+					101
+				</p>
+				<h3 class="symbol">Md</h3>
+				<h2 class="name">Mendelevium</h2>
+				<p class="weight">
+					(258)
+				</p>
+			</div>
+
+			<div class="element opa halogen nonmetal" data-symbol="Cl" data-category="halogen">
+				<p class="number">
+					17
+				</p>
+				<h3 class="symbol">Cl</h3>
+				<h2 class="name">Chlorine</h2>
+				<p class="weight">
+					35.453
+				</p>
+			</div>
+
+			<div class="element opa noble-gas nonmetal" data-symbol="Ar" data-category="noble-gas">
+				<p class="number">
+					18
+				</p>
+				<h3 class="symbol">Ar</h3>
+				<h2 class="name">Argon</h2>
+				<p class="weight">
+					39.948
+				</p>
+			</div>
+
+			<div class="element opa alkali metal" data-symbol="K" data-category="alkali">
+				<p class="number">
+					19
+				</p>
+				<h3 class="symbol">K</h3>
+				<h2 class="name">Potassium</h2>
+				<p class="weight">
+					39.0983
+				</p>
+			</div>
+			<div class="element opa actinoid metal inner-transition" data-symbol="Np" data-category="actinoid">
+				<p class="number">
+					93
+				</p>
+				<h3 class="symbol">Np</h3>
+				<h2 class="name">Neptunium</h2>
+				<p class="weight">
+					(237)
+				</p>
+			</div>
+		</div>
+
+		<nav id="page_nav">
+			<a href="isotope-site/pages/2.html"></a>
+		</nav>
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<footer style="position: fixed; bottom:0; z-index: 200; background-color: #000; height:150px; width:100%">
+			<div>
+				patrocinadores
+			</div>
+			<div>
+				facebook
+			</div>
+			<div>
+				direitos reservados
+			</div>
+		</footer>
+		<script type="text/javascript" src="js/jquery-1.7.1.min.js"></script>
+		<script type="text/javascript" src="js/jquery.isotope.min.js"></script>
+		<script type="text/javascript" src="js/jquery.imagesloaded.min.js"></script>
+		<script type="text/javascript" src="js/jquery.infinitescroll.min.js"></script>
+		<script type="text/javascript" src="js/jquery.scrollTo-min.js"></script>
+		<script type="text/javascript" src="js/jquery.reveal.js"></script>
+
+		<script type="text/javascript">
+			/*
+			 $.Isotope.prototype._getCenteredMasonryColumns = function() {
+			 this.width = this.element.width();
+
+			 var parentWidth = this.element.parent().width();
+
+			 // i.e. options.masonry && options.masonry.columnWidth
+			 var colW = this.options.masonry && this.options.masonry.columnWidth ||
+			 // or use the size of the first item
+			 this.$filteredAtoms.outerWidth(true) ||
+			 // if there's no items, use size of container
+			 parentWidth;
+
+			 var cols = Math.floor(parentWidth / colW);
+			 cols = Math.max(cols, 1);
+
+			 // i.e. this.masonry.cols = ....
+			 this.masonry.cols = cols;
+			 // i.e. this.masonry.columnWidth = ...
+			 this.masonry.columnWidth = colW;
+			 };
+
+			 $.Isotope.prototype._masonryReset = function() {
+			 // layout-specific props
+			 this.masonry = {};
+			 // FIXME shouldn't have to call this again
+			 this._getCenteredMasonryColumns();
+			 var i = this.masonry.cols;
+			 this.masonry.colYs = [];
+			 while (i--) {
+			 this.masonry.colYs.push(0);
+			 }
+			 };
+
+			 $.Isotope.prototype._masonryResizeChanged = function() {
+			 var prevColCount = this.masonry.cols;
+			 // get updated colCount
+			 this._getCenteredMasonryColumns();
+			 return (this.masonry.cols !== prevColCount );
+			 };
+
+			 $.Isotope.prototype._masonryGetContainerSize = function() {
+			 var unusedCols = 0, i = this.masonry.cols;
+			 // count unused columns
+			 while (--i) {
+			 if (this.masonry.colYs[i] !== 0) {
+			 break;
+			 }
+			 unusedCols++;
+			 }
+
+			 return {
+			 height : Math.max.apply(Math, this.masonry.colYs),
+			 // fit container to columns that have been used;
+			 width : (this.masonry.cols - unusedCols) * this.masonry.columnWidth
+			 };
+			 };
+
+			 */
+			$(function() {
+				$('a[data-reveal-id]').click(function(){
+					defineEventName($(this).attr('data-event-name'));
+				});
+				
+				function defineEventName(eventName){
+					$('#eventName').val("");
+					$('#eventName').val(eventName);
+				}
+				
+				$(':input[title]').each(function() {
+					var $this = $(this);
+					if ($this.val() === '') {
+						$this.val($this.attr('title'));
+					}
+					$this.focus(function() {
+						if ($this.val() === $this.attr('title')) {
+							$this.val('');
+						}
+					});
+					$this.blur(function() {
+						if ($this.val() === '') {
+							$this.val($this.attr('title'));
+						}
+					});
+				});
+
+				var $container = $('#container');
+				var $columns = 22;
+
+				// initialize Isotope
+				$container.imagesLoaded(function() {
+					$container.isotope({
+						// options...
+						resizable : false, // disable normal resizing
+						// set columnWidth to a percentage of container width
+						masonry : {
+							columnWidth : 160//$container.width() / $columns
+						}
+					});
+				});
+
+				// update columnWidth on window resize
+				$(window).smartresize(function() {
+					$container.isotope({
+						// update columnWidth to a percentage of container width
+						masonry : {
+							columnWidth : 160//$container.width() / $columns
+						}
+					});
+				});
+
+				$container.infinitescroll({
+					navSelector : '#page_nav', // selector for the paged navigation
+					nextSelector : '#page_nav a', // selector for the NEXT link (to page 2)
+					itemSelector : '.element', // selector for all items you'll retrieve
+					loading : {
+						finishedMsg : 'Todos eventos carregados...',
+						img : 'http://i.imgur.com/qkKy8.gif',
+						msgText : 'Carregando mais eventos...',
+						donetext : "Todos eventos carregados",
+						loadingText : 'Carregando mais eventos...',
+						animate : false
+					}
+				},
+				// call Isotope as a callback
+				function(newElements) {
+					$container.isotope('appended', $(newElements), function() {
+						$(newElements).css({
+							opacity : ''
+						})
+						$(newElements).addClass('opa');
+					});
+				});
+
+				//$("div.element").removeClass('expand').addClass('opa');
+				$("div.element").live('click', function() {
+					$this = $(this);
+
+					if($this.hasClass('expand')){return;};
+
+					$("div.element").removeClass('expand').addClass('opa');
+					$this.toggleClass('expand').removeClass('opa');
+
+					$("div.element").find('.littleContent').show();
+					$("div.element").find('.bigContent').hide();
+
+					$this.find('.littleContent').hide();
+					$this.find('.bigContent').fadeIn();
+
+					$container.isotope('reLayout');
+				});
+
+			});
+		</script>
+
+	</body>
+</html>
